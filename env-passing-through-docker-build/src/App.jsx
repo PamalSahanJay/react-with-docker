d74a0251b83d824e44875354c0dd42b3cdb56a03
psahan <psahan@wiley.com>
2024-08-31 12:01:15 +0530

    change docker file

--- env-passing-through-docker-build/src/App.jsx
+++ env-passing-through-docker-build/src/App.jsx
@@ -1,33 +1,38 @@
 import { useState } from 'react'
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+function Home() {
+  return (
+    <div>
+      <h1>client id : {import.meta.env.VITE_OIDC_CLIENT_ID}</h1>
+      <h1>client secreat : {import.meta.env.VITE_OIDC_CLIENT_SECRET}</h1>
+      <h1>authority url : {import.meta.env.VITE_OIDC_AUTHORITY}</h1>
+    </div>
+  );
+}
+
+function About() {
+  return (
+    <div>
+      <h1>About Page</h1>
+      <p>This is the about page.</p>
+    </div>
+  );
+}
+
 function App() {
   const [count, setCount] = useState(0)
-  console.log(import.meta.env.VITE_NAME)
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </Router>
     </>
   )
 }
